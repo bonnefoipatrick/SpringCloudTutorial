301b92b8626654bc819d48370c20b43cf60effd3
BONNEFOI Patrick <bonnefoipatrick@yahoo.fr>
2018-05-01 14:02:22 +0200

    Ajout du module SpringCloudConfig

---
+++ SpringCloudConfig/src/main/java/com/cis/springcloud/configuration/SpringCloudConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.cis.springcloud.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigurationApplication.class, args);
+	}
+}
